651e463cdc05d5e4774134528832708d1be3d47a
maciejors <maciej.orslowski@gmail.com>
2025-05-28 14:12:42 +0200

    add the final script for the first experiment

---
+++ scripts/experiment_ppo_compare_reward_settings.py
@@ -0,0 +1,25 @@
+from sb3_contrib.ppo_mask import MaskablePPO
+
+from hearts_ai.rl.training import train_playing_agent
+
+for i in range(5):
+    train_playing_agent(
+        agent_cls=MaskablePPO,
+        env_kwargs={'reward_setting': 'dense'},
+        log_path='output/logs/ppo_compare_reward_settings/dense',
+        stages_lengths_episodes=[192 * 500] * 6,
+        eval_freq_episodes=10000,
+        n_eval_episodes=10000,
+        progress_bar=True,
+        random_state=i,
+    )
+    train_playing_agent(
+        agent_cls=MaskablePPO,
+        env_kwargs={'reward_setting': 'sparse'},
+        log_path='output/logs/ppo_compare_reward_settings/sparse',
+        stages_lengths_episodes=[192 * 500] * 6,
+        eval_freq_episodes=10000,
+        n_eval_episodes=10000,
+        progress_bar=True,
+        random_state=i,
+    )
