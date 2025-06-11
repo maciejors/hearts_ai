3891a8a21df4ff4dab2bf5fe1018915bccff8632
maciejors <maciej.orslowski@gmail.com>
2025-06-11 08:12:19 +0200

    add results for experiment 2

--- hearts_ai/rl/results/__init__.py
+++ hearts_ai/rl/results/__init__.py
@@ -1,9 +1,10 @@
 from .load import load_training_results
-from .training_results import TrainingResults
+from .training_results import TrainingResults, print_best_eval_run
 from .visualisation import PlotMakerPlaying, PlotMakerCardPassing
 
 __all__ = [
     'load_training_results',
+    'print_best_eval_run',
     'TrainingResults',
     'PlotMakerPlaying',
     'PlotMakerCardPassing',
--- hearts_ai/rl/results/training_results.py
+++ hearts_ai/rl/results/training_results.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Literal
 
 import pandas as pd
 
@@ -9,3 +10,25 @@ class TrainingResults:
     eval_results: dict[str, pd.DataFrame]
     training_logs: pd.DataFrame
     stages_ends: list[int]
+
+
+def print_best_eval_run(results: TrainingResults, eval_id: Literal['random', 'rule_based']) -> None:
+    eval_df = results.eval_results[eval_id]
+    eval_df = eval_df[['run_id', 'eval_no', 'reward']]
+
+    eval_mean_df = eval_df \
+        .groupby(['run_id', 'eval_no']) \
+        .mean() \
+        .reset_index()
+    eval_std_df = eval_df \
+        .groupby(['run_id', 'eval_no']) \
+        .std() \
+        .reset_index()
+
+    best_eval_idx = eval_mean_df['reward'].argmax()
+    best_eval_run_id = eval_mean_df.iloc[best_eval_idx]['run_id']
+    best_eval_reward_value = eval_mean_df.iloc[best_eval_idx]['reward']
+    best_eval_reward_std = eval_std_df.iloc[best_eval_idx]['reward']
+
+    print(f"The best evaluation occurred during '{best_eval_run_id}' "
+          f'(episode_reward={best_eval_reward_value:.2f} +/- {best_eval_reward_std:.2f})')
