e250198877c915ddc81f5dedc7c51b02a362cdd2
maciejors <maciej.orslowski@gmail.com>
2025-06-12 20:55:37 +0200

    update training script for expreiment 3

--- scripts/experiment_ppo_passing_compare_stages_setups.py
+++ scripts/experiment_ppo_passing_compare_stages_setups.py
@@ -5,7 +5,7 @@ from hearts_ai.rl.training import train_card_passing_agent
 best_playing_agent = MaskablePPO.load(
     'output/logs/ppo_playing_compare_stages_setups/every_192x100/run_8/eval_rule_based/best_model'
 )
-for i in range(10):
+for i in range(2, 5):
     train_card_passing_agent(
         agent_cls=MaskablePPO,
         playing_agent=best_playing_agent,
@@ -13,7 +13,7 @@ for i in range(10):
         log_path='output/logs/ppo_passing_compare_stages_setups/every_512x2',
         eval_freq_episodes=2000,
         n_eval_episodes=500,
-        stages_lengths_episodes=[512 * 2] * 20,
+        stages_lengths_episodes=[512 * 2] * 40,
         progress_bar=True,
         random_state=i,
     )
@@ -21,21 +21,10 @@ for i in range(10):
         agent_cls=MaskablePPO,
         playing_agent=best_playing_agent,
         env_kwargs={},
-        log_path='output/logs/ppo_passing_compare_stages_setups/every_512x5',
+        log_path='output/logs/ppo_passing_compare_stages_setups/every_512x20',
         eval_freq_episodes=2000,
         n_eval_episodes=500,
-        stages_lengths_episodes=[512 * 5] * 8,
-        progress_bar=True,
-        random_state=i,
-    )
-    train_card_passing_agent(
-        agent_cls=MaskablePPO,
-        playing_agent=best_playing_agent,
-        env_kwargs={},
-        log_path='output/logs/ppo_passing_compare_stages_setups/every_512x10',
-        eval_freq_episodes=2000,
-        n_eval_episodes=500,
-        stages_lengths_episodes=[512 * 10] * 4,
+        stages_lengths_episodes=[512 * 20] * 4,
         progress_bar=True,
         random_state=i,
     )
