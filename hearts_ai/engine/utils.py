fix winnder idx calculation

hearts_ai/engine/utils.py
@@ -17,7 +17,8 @@ def points_for_card(card_idx: int | np.int_) -> int:
 
 def get_winning_card_argmax(cards: np.ndarray, leading_suit: int) -> np.int32:
     cards_matching_lead_suits = cards[cards // 13 == leading_suit]
-    return np.argmax(cards_matching_lead_suits)
+    winning_card = np.max(cards_matching_lead_suits)
+    return np.where(cards == winning_card)[0].item()
 
 
 def get_valid_plays(hand: np.ndarray,